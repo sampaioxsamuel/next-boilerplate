641b454bb6e5548418449191c26b50ea8e1f15d9
Samuel Nogueira <sampaioxsamuel@gmail.com>
2021-07-04 17:01:07 -0300

    add rtl

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,8 +4,10 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(screen.getByRole('heading', { name: /react/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import Home from '.'
-
-describe('<Home />', () => {
-  it('should show next title', () => {
-    render(<Home />)
-  })
-})
